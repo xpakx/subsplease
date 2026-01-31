d1d245071ec1f3e801c1f9cd459c7d2f0e32276f
xpakx <55586470+xpakx@users.noreply.github.com>
2026-01-31 23:52:41 +0100

    feat: add arg flags for weekly schedule and latest eps

--- subsplease/main.py
+++ subsplease/main.py
@@ -4,6 +4,7 @@ from metadata import MetadataProvider
 from db import AnimeDB
 from utils import today, schedule
 from torrent import send_magnet_to_transmission
+from display import display_schedule
 import subprocess
 import time
 import argparse
@@ -11,21 +12,27 @@ import argparse
 
 def main():
     pass
-    # latests = subs.latest()
-    # latests = latests.unwrap()
     # send_magnet_to_transmission(latests[0], '720')
-
-    # display_schedule(res.unwrap().schedule.monday)
     # res = subs.search("frieren")
     # print(res.unwrap()[0])
     # magnet(latests[0], '480')
 
+
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="")
     parser.add_argument("-t", "--tracked", action="store_true", help="Show only tracked")
+    parser.add_argument("-l", "--latest", action="store_true", help="Get latest episodes")
+    parser.add_argument("-w", "--weekly", action="store_true", help="Show weekly schedule")
 
     args = parser.parse_args()
     meta = MetadataProvider()
     db = AnimeDB()
     subs = Subsplease()
-    today(meta, db, subs, only_tracked=args.tracked)
+    if args.latest:
+        data = subs.latest()
+        print(data)
+    elif args.weekly:
+        data = subs.weekly_schedule()
+        display_schedule(res.unwrap().schedule.monday)
+    else:
+        today(meta, db, subs, only_tracked=args.tracked)
