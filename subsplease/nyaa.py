d7ad9a49a33af8c8a4e89386d6eca618f82dc9bb
xpakx <55586470+xpakx@users.noreply.github.com>
2026-02-02 20:56:11 +0100

    feat: reconstruct magnet link from hash and create torrent file

--- subsplease/nyaa.py
+++ subsplease/nyaa.py
@@ -1,9 +1,37 @@
 import feedparser
+import urllib
+from result import Result, Ok
+from dataclasses import dataclass
 
 
-def nyaa_newest(query: str):
+@dataclass
+class NyaaTorrent:
+    title: str
+    seeders: str
+    leechers: str
+    size: str
+    magnet: str
+
+
+def create_magnet(info_hash, title):
+    magnet_data = {
+        'xt': f'urn:btih:{info_hash}',
+        'dn': title,
+        'tr': [
+            'http://nyaa.tracker.wf:7777/announce',
+            'udp://open.stealth.si:80/announce',
+            'udp://tracker.opentrackr.org:1337/announce',
+            'udp://exodus.desync.com:6969/announce',
+            'udp://tracker.torrent.eu.org:451/announce'
+        ]
+    }
+    return f"magnet:?{urllib.parse.urlencode(magnet_data, doseq=True)}"
+
+
+def nyaa_newest(query: str) -> Result[list[NyaaTorrent], str]:
     rss_url = f"https://nyaa.si/?page=rss&q={query}&c=1_2&f=0"
     feed = feedparser.parse(rss_url)
+    results = []
 
     for entry in feed.entries:
         print(f"Title: {entry.title}")
@@ -13,8 +41,14 @@ def nyaa_newest(query: str):
         info_hash = entry.get('nyaa_infohash', 'N/A')
         size = entry.get('nyaa_size', 'N/A')
 
-        print(f"Seeders: {seeders}: {leechers}")
-        print(f"Leechers: {leechers}")
-        print(f"Hash: {info_hash}")
-        print(f"Size: {size}")
-        print("-" * 20)
+        results.append(
+                NyaaTorrent(
+                    title=entry.title,
+                    seeders=seeders,
+                    leechers=leechers,
+                    size=size,
+                    magnet=create_magnet(info_hash, entry.title)
+                )
+        )
+    print(results)
+    return Ok(results)
