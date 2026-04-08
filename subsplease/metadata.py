4d5ed7c6e9990e2c57ec74b983a05020ea4a109b
xpakx <55586470+xpakx@users.noreply.github.com>
2026-04-08 20:38:14 +0200

    feat: add option to get whole season summary from anilist

--- subsplease/metadata.py
+++ subsplease/metadata.py
@@ -2,6 +2,7 @@ import msgspec
 import requests
 from subsplease.result import Result, Err, Ok
 from typing import Generic, TypeVar
+from datetime import datetime
 
 T = TypeVar("T")
 
@@ -12,6 +13,18 @@ class AniListData(msgspec.Struct, Generic[T], rename='pascal'):
 
 class AniListResponse(msgspec.Struct, Generic[T]):
     data: AniListData[T]
+
+
+class AniListDataPage(msgspec.Struct, Generic[T]):
+    media: T
+
+
+class AniListDataPaged(msgspec.Struct, Generic[T], rename='pascal'):
+    page: AniListDataPage[T]
+
+
+class AniListResponsePaged(msgspec.Struct, Generic[T]):
+    data: AniListDataPaged[T]
 
 
 class AniListTitle(msgspec.Struct):
@@ -211,3 +224,67 @@ class MetadataProvider:
             return Ok(data.data.media)
         except msgspec.DecodeError as e:
             return Err(f"Decode error: {e}")
+
+    def _season_name(self) -> tuple[str, int]:
+        now = datetime.now()
+        year = now.year
+        month = now.month
+        if month in [1, 2, 3]:
+            return "WINTER", year
+        elif month in [4, 5, 6]:
+            return "SPRING", year
+        elif month in [7, 8, 9]:
+            return "SUMMER", year
+        else:
+            return "FALL", year
+
+    def get_current_season_summary(self):
+        season, year = self._season_name()
+
+        query = """
+        query ($season: MediaSeason, $seasonYear: Int) {
+          Page(page: 1, perPage: 50) {
+            media(season: $season, seasonYear: $seasonYear, type: ANIME, sort: POPULARITY_DESC) {
+              id
+              title {
+                romaji
+                english
+                native
+              }
+              description(asHtml: false)
+              status
+              nextAiringEpisode {
+                  airingAt
+                  episode
+                  }
+              tags { name }
+            }
+          }
+        }
+        """
+
+        response = requests.post(
+            self.url,
+            json={
+                'query': query,
+                'variables': {'season': season, "seasonYear": year}
+            }
+        )
+
+        if response.status_code != 200:
+            print(response.content)
+            return Err(f"AniList API Error: {response.status_code}")
+
+        try:
+            data = msgspec.json.decode(
+                    response.content,
+                    type=AniListResponsePaged[list[AniListMediaDetails]]
+            )
+            return Ok(data.data.page.media)
+        except msgspec.DecodeError as e:
+            return Err(f"Decode error: {e}")
+
+
+if __name__ == "__main__":
+    meta = MetadataProvider()
+    print(meta.get_current_season_summary())
