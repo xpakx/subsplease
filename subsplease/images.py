0b5c1da04cb87c01c2c8f47498d96b9a990ca345
xpakx <55586470+xpakx@users.noreply.github.com>
2026-04-26 22:31:28 +0200

    chore: add error messages for clip searching

--- subsplease/images.py
+++ subsplease/images.py
@@ -11,12 +11,12 @@ class ImageService:
         tags = self.sakuga.search_tag(formatted_name)
         # TODO: save tag in repo
         if tags.is_err():
+            print(f"Couldn't find sakugabooru tag for {show.title_english}")
             return
         tag_list = tags.unwrap()
         if len(tag_list) == 0:
+            print(f"No clips yet for {show.title_english}")
             return
-        print(tag_list)
         tag = tag_list[0]
         posts = self.sakuga.find_posts(tag.name).unwrap()
-        print(posts[0])
         self.sakuga.download_images(posts)
